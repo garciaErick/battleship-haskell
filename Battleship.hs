bf9f9bb0ef4961fd70af015ed0505eb6dad734d6
Erick Garcia <egarcia87@miners.utep.edu>
2015-11-24 23:54:42 -0600

    progress was made

--- Battleship.hs
+++ Battleship.hs
@@ -4,134 +4,96 @@ module Battleship where
 	import System.IO
 	
 	type Board = [[Int]]
-	type Board1 = [[Integer]]
 	type Coordinates = (Int, Int, Bool)
 	type Coordinate = (Int, Int)
 
-
-	getBoolean :: Bool -> [Char]
-	getBoolean n = 
-		if n == True
-			then "You have placed a ship"
-		else 
-			"Invalid Coordinates"
-
-	mkBoard :: Int -> Board
-	mkBoard 0 = [[]]
-	mkBoard n = 
-		if n >= 0 
-			then take n (repeat (take n (repeat 0)))
-		else [[0]]
-	list = [0,0,0,0,0,0,0,0,0,0]
-
-	mkBoard1 :: [Integer] -> [Integer] -> [Integer] -> [Integer] ->
-				[Integer] -> [Integer] -> [Integer] -> [Integer] -> 
-				[Integer] -> [Integer] -> Board1
-	mkBoard1 row1 row2 row3 row4 row5 row6 row7 row8 row9 row10 = [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10]
-	board1 = mkBoard1 list list list list list list list list list list 
-
-	mkBoard2 [[rows]] = [[rows]]
-	updatedBoard n newRow board= board1
-
-					--row0 = getRow 0 board
-	     			--row1 = getRow 1 board
-	     			--row2 = getRow 2 board
-	     			--row3 = getRow 3 board
-	     			--row4 = getRow 4 board
-	     			--row5 = getRow 5 board
-	     			--row6 = getRow 6 board
-	     			--row7 = getRow 7 board
-	     			--row8 = getRow 8 board
-	     			--row9 = getRow 9 board
-	--mkBoard1 n = 
-	--	if n >= 0 
-	--		then take n (repeat (take n (repeat 0)))
-	--	else [[0]]
-
-	board = mkBoard 10
-	getRow n board = board !! n
-	--printRow n = [ toString x | x <- getRow n]
-	--placeShip ::  Coordinate -> Board
-	tuple1 = (1,2,True)
-	getX (x,_,_) = x
-	getY (_,y,_) = y
-	getDir (_,_,dir) = dir
-	--isShipPlaceable n x y dir board
+	--Replacing a value in a list
+	replace index newVal (x:xs)
+	     | index == 0 = newVal:xs
+	     | otherwise = x:replace (index-1) newVal xs
+	--Print Regular Board
+	printBoard :: [[Int]] -> IO ()
+	printBoard = putStrLn . unlines . listValues
+	listValues :: [[Int]] -> [String]
+	listValues = map (map valueOf)
+	valueOf :: Int -> Char
+	valueOf 0 = '0' -- Default Place
+	valueOf 1 = '0' -- Minesweeper
+	valueOf 2 = '0' -- Submarine
+	valueOf 3 = '0' -- Frigate
+	valueOf 4 = '0' -- Battleship
+	valueOf 5 = '0' -- Aircraft Carrier
+	valueOf 6 = 'x' -- Place Hit
+	valueOf _ = 'x' -- Place Missed
+	--Print Cheat Board  
+	printCheatBoard :: [[Int]] -> IO ()
+	printCheatBoard = putStrLn . unlines . listCheatValues
+	listCheatValues :: [[Int]] -> [String]
+	listCheatValues = map (map cheatValue)
+	cheatValue :: Int -> Char
+	cheatValue 0 = '0' -- Default Place
+	cheatValue 1 = 'M' -- Minesweeper
+	cheatValue 2 = 'S' -- Submarine
+	cheatValue 3 = 'F' -- Frigate
+	cheatValue 4 = 'B' -- Battleship
+	cheatValue 5 = 'A' -- Aircraft Carrier
+	cheatValue 6 = 'x' -- Place Hit
+	cheatValue _ = 'x' -- Place Missed
+	--For parsing user input
 	convertStringToCoordinates :: String -> Coordinate
 	convertStringToCoordinates ['(', x, ',', y, ')'] = ((ord x) - (ord '0') + 1, (ord y) - (ord '0') + 1)
 	convertStringToCoordinates _ = (-1, -1)
-
-	list1 = [1,2,3,4,5,6,7,8,9,10]
-	replaceValues n x y ship board
-	     | index == x = return newBoard
+	getRow n board = board !! n
+	--Board Functions
+	--List to initiate Board
+	list = take 10 [0,0..0]
+	mkBoard :: [Int] -> [Int] -> [Int] -> [Int] ->
+				[Int] -> [Int] -> [Int] -> [Int] -> 
+				[Int] -> [Int] -> Board
+	mkBoard row1 row2 row3 row4 row5 row6 row7 row8 row9 row10 = [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10]
+	board = mkBoard list list list list list list list list list list 
+	--Ship Functions
+	--Insert a [Int] at a [[Int]] (Board)
+	insertRowAt :: Int -> [Int]-> Board -> Board 
+	insertRowAt z y board = as ++ [y] ++ cs
+                where 
+                  	(as,bs) = splitAt z board
+                	cs = drop 1 bs
+	placeShipx n x y ship board
+	     | index == x = newBoard
 	     | otherwise = 
 	     	do 
-	     		replaceValues (n-1) x y ship newBoard
+	     		placeShipx (n-1) x y ship newBoard
 	     		where 
 	     			newBoard = insertRowAt y newRow board
 	     			newRow = replace index ship row
 	     			row = getRow y board
 	     			index = (n-1)+x
-
-	placeShip n x y ship board = [replace x ship row]
-		where 
-			row = board !! y
-			ship = 1
-
-
-	replace index newVal (x:xs)
-	     | index == 0 = newVal:xs
-	     | otherwise = x:replace (index-1) newVal xs
-
-	-- Cheat Places     
-	valueOf :: Int -> Char
-	valueOf 0 = '0' -- Default Place
-	valueOf 1 = 'M' -- Minesweeper
-	valueOf 2 = 'S' -- Submarine
-	valueOf 3 = 'F' -- Frigate
-	valueOf 4 = 'B' -- Battleship
-	valueOf 5 = 'A' -- Aircraft Carrier
-	valueOf 6 = 'x' -- Place Hit
-	valueOf _ = 'x' -- Place Missed
-
-	printBoard :: [[Int]] -> IO ()
-	printBoard = putStrLn . unlines . listValues
-	listValues :: [[Int]] -> [String]
-	listValues = map (map valueOf)
-
+	hit x y board = insertRowAt y newRow board	
+			where
+				newRow = replace x 6 (getRow y board)
 	isShipPlaceable :: Coordinates -> Bool
 	isShipPlaceable (x,y,dir) = if x < 1 || x > 10 || y < 1 || y > 10 then False
 	                            else True
+    --
+    --isShip x y board =
+    --	if 
+	getBoolean n = 
+  		if n == True
+			then "1"
+		else 
+			"0"
+			where n = n+1
 
-	placeShip1 :: Coordinates -> [Char]
-	placeShip1 (x,y,dir) = if isShipPlaceable (x,y,dir) == False then getBoolean(False)
-	                      else getBoolean(True)
+    --Do not Erase
+	--getBoolean :: Bool -> [Char]
+	--getBoolean n = 
+	--	if n == True
+	--		then "You have placed a ship"
+	--	else 
+	--		"Invalid Coordinates"
 
- --   play :: [Board] -> [[Ship]] -> IO ()
-	--play board ships = do
- --       printField (last board) (last ships)
- --       (newField, newShipList) <- fireWithEveryShip (last board, last ships) 1
- --       if length newShipList == 0 then
- --           do
- --             putStrLn ("\n Game over!\n")
- --             printField newField newShipList
- --       else
- --           play [newField] [newShipList]
-
- --   inputShip :: [Ship] -> Int -> IO Ship
-	--inputShip placedShips len = do
-	--    putStrLn ("Enter the coordinates of the ship of length " ++ show len ++ "?")
-	--    string <- getLine
-	--    let stringCoords = splitCoordinatesInString string
-	--    let coords = map convertStringToCoordinates stringCoords
-	--    if validateShipCoordinates placedShips coords len then
-	--        return coords
-	--    else
-	--        inputShip placedShips len
-	--let (as,bs) = splitAt z xs   in   as ++ [new_element] ++ bs
-	insertRowAt :: Int -> [Integer]-> Board1 -> Board1 
-	insertRowAt z y board = as ++ [y] ++ cs
-                where 
-                  	(as,bs) = splitAt z board
-                	cs = drop 1 bs
-	--let (ys,zs) = splitAt n xs   in   ys ++ (tail zs)
+	--tuple1 = (1,2,True)	
+	--getX (x,_,_) = x
+	--getY (_,y,_) = y
+	--getDir (_,_,dir) = dir
